73e9096d4721d46bfeaf67fda0069b3ec800854c
jing <jingli1524@gmail.com>
2023-01-26 17:22:45 -0500

    threejs initial setup with dummy cube

---
+++ components/PlayerCar.tsx
@@ -0,0 +1,9 @@
+const PlayerCar = () => {
+  return (
+    <>
+      <h1>playerCar</h1>
+    </>
+  )
+}
+
+export default PlayerCar
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,63 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import PlayerCar from '@/components/PlayerCar'
+import * as Three from 'three'
+import { useEffect, useRef } from 'react'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
 
 export default function Home() {
+  const canvasRef = useRef()
+  useEffect(() => {
+    const scene = new Three.Scene()
+    const camera = new Three.PerspectiveCamera(
+      50,
+      window.innerWidth / window.innerHeight,
+      1,
+      1000
+    )
+    camera.position.z = 96
+
+    //const canvas = document.getElementById('myThreeJsCanvas')
+    const canvas = canvasRef.current
+    console.log({ canvas })
+    const renderer = new Three.WebGLRenderer({ canvas, antialias: true })
+    renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(renderer.domElement)
+
+    const ambientLight = new Three.AmbientLight(0xffffff, 0.5)
+    ambientLight.castShadow = true
+    scene.add(ambientLight)
+
+    const spotLight = new Three.SpotLight(0xffffff, 1)
+    ambientLight.castShadow = true
+    spotLight.position.set(0, 64, 32)
+    scene.add(spotLight)
+
+    const boxGeometry = new Three.BoxGeometry(16, 16, 16)
+    const boxMaterial = new Three.MeshNormalMaterial()
+    const boxMesh = new Three.Mesh(boxGeometry, boxMaterial)
+    scene.add(boxMesh)
+
+    //orbit control
+    const controls = new OrbitControls(camera, renderer.domElement)
+
+    //add frames per second stat
+    const stats = Stats()
+    document.body.appendChild(stats.dom)
+
+    const animate = () => {
+      boxMesh.rotation.x += 0.01
+      boxMesh.rotation.y += 0.01
+      stats.update()
+      controls.update()
+      renderer.render(scene, camera)
+      window.requestAnimationFrame(animate)
+    }
+    animate()
+  }, [])
+
   return (
     <>
       <Head>
@@ -15,108 +68,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
+        {/* @ts-ignore */}
+        <canvas id="myThreeJsCanvas" ref={canvasRef} />
+        {/* <PlayerCar /> */}
       </main>
     </>
   )
